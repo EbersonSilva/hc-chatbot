2438e4e5614f66f2da4a1261624e9f2d46c9effd
Eberson Rodrigo Matos da Silva <eberson.silva@pagg.local>
2025-08-04 14:30:08 -0300

    Atualização do front e junção com back

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+390f6b03724fc6468260b7812980e966df88aaa0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f74c82c82a398e4fd99392f766603b2f1081ad03")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
